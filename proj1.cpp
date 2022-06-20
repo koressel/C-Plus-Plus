56fba938a047a7bcfcbe15f6af9c1683302e3b24
Lorkor <koressel.dev@gmail.com>
2022-06-20 13:52:02 -0500

    Removed 3d vector array code

--- proj1.cpp
+++ proj1.cpp
@@ -1,8 +1,60 @@
-#include <stdio.h>
+// COP 2224 Section 1877 Project 1 - Appointment Calendar
+// ================================
+
+// See due date in Canvas
+
+// You are going to create a simple appointment calendar program using the skills that you have picked up in class so far. You are not to use any libraries that import calendar information -- please use only those elements that are found in Chapters 1-7 of your textbook. 
+
+// _Functionality__
+
+// 1. When the program starts, the user should be asked to enter a four-digit year from 2000 to 2100. If the user inputs an invalid year, then the program should tell them that and allow them to choose another. Once a year is chosen, they do not ever have to chose another for this run of the program.
+
+// 2. After the user has successfully entered a year, they should be shown a menu whose choices include (1) Picking a month (this can be by number or by name -- programmer choice). (2) Printing out all appointments to screen (see below) (3) Writing appointments to file (see below) or (4) Quitting the program.  Invalid input should again prompt the user to re-enter a value.
+
+// 3. Upon choosing a month, the program should print to the screen a simple and ACCURATE text calendar similar to those found at  https://beautifuldingbats.com/calendar-generator/ (a sample is below) showing month, year, days of the week, and a grid with all of the days of that particular month. It must be correct for leap years (remember the rules -- 2000 is different from 2100). Your program should generate the month dynamically based upon knowing how many days there are in each month and what day a given month starts (functions will help to determine starting day, and the modulus operator will help in printing the grid).
+
+// ┌────────────────────┐
+// │ February ▒▒▒▒ 2020 │
+// ├──┬──┬──┬──┬──┬──┬──┤
+// │Su│Mo│Tu│We│Th│Fr│Sa│
+// ├──┼──┼──┼──┼──┼──┼──┤
+// │▒▒│▒▒│▒▒│▒▒│▒▒│▒▒│01│
+// ├──┼──┼──┼──┼──┼──┼──┤
+// │02│03│04│05│06│07│08│
+// ├──┼──┼──┼──┼──┼──┼──┤
+// │09│10│11│12│13│14│15│
+// ├──╔══╗──┼──┼──┼──┼──┤
+// │16║17║18│19│20│21│22│
+// ├──╚══╝──┼──┼──┼──┼──┤
+// │23│24│25│26│27│28│29│
+// └──┴──┴──┴──┴──┴──┴──┘
+
+// 4. After the visual calendar, the user should be presented with a menu showing three choices: (1) To go back to the previous menu that will allow them to pick a month (2) To create an appointment for a given day of the current month. (3) To quit the program. 
+
+// 5. If they choose to create an appointment, they should be able to pick a day to set the appointment. Once they pick a day, they should be able to type some short text for the appointment. It is *not* necessary to choose a time. If they have already written some text into an appointment for that day, they should be able to see the previous entry and add to it but not delete or edit it. When they finish writing their appointment note, they should be taken back to that month's menu and the text calendar should now indicate that the chosen day has an appointment. A given month should be able to have an appointment for any given day. Once a month has any appointments, the menu should add an option to print them to screen.
+
+// 6. The menu in #2 above should have an option to print all appointments for a given year to screen at once. This could simply be the same call as for a single month in #5 but called twelve times (once for each month).
+
+// 7. The menu in #2 above should also have an option to write all the appointments to files --one for each month. The file names should reflect month and year.
+
+
+// _Grading_
+
+// This assignment is worth 100 points and will be graded based upon how well it implements the features listed above. A program that implements all of them will get 100 points, and the instructor reserves the right to compensate for points marked off with additional points if other features are implemented beyond the bar minimum listed. 
+
+// You should turn in a single .cpp file that I can compile and run. The filename should include your name and that it belongs to project 1. The same information should be in comments at the top of the code. 
+
+// _Helpful Hints_
+
+// We strongly suggest sitting down and designing this program on paper before writing the code. Everything you need is in the book. Menus are probably best accomplished with switch statements. Holding year, month, and appointment data probably involved multiple arrays (even nested or "three dimensional" arrays). The input, output, and file options can all be accomplished using the basic methods you have already encountered. Breaking down the different tasks into functions is highly advised as there are many parts of this program that should be reused. Do not use global variables, but keep as many arrays in your "main" function as you need to have appointment data always available. Above all, do not overthink this program -- the functionality we are asking for is quite simple and can be built piece-by-piece. 
+
+
 #include <iostream>
+#include <stdio.h>
 #include <string>
 #include <iomanip>
 #include <stdlib.h>
+#include<vector>
 using namespace std;
 
 int printCalendar(long calendar[100][12][42]) {
@@ -50,7 +102,6 @@ int printMainMenu(int year) {
     return 0;
 }
 
-
 int printMonthSelectMenu(int year) {
     std::system("clear");
     cout << setw(15) << "\nMonth Select Menu (Year " << year << ")\n";
@@ -72,7 +123,7 @@ int printMonthSelectMenu(int year) {
     return 0;
 }
 
-int printCalendarMonth(int year, int month, long century[100][12][42], string appts[100][12][42]) {
+int printCalendarMonth(int year, int month, long century[100][12][42]) {
     int y = year; int m = month;
     string monthNames[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
     string weekdayNames[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
@@ -88,9 +139,9 @@ int printCalendarMonth(int year, int month, long century[100][12][42], string ap
     cout << "\n";
     
     for (int day = 0; day < 42; day++) {
-        if (appts[year-2000][month-1][day] != "XX") {
-            modifier = "*";
-        }
+        // if (appts[year-2000][month-1][day] != "XX") {
+        //     modifier = "*";
+        // }
         cout << setw(4) << century[year-2000][month-1][day] << modifier;
         if(day==6 || day==13 || day==20 || day==27 || day==34) {
             cout << "\n";
@@ -123,14 +174,6 @@ int main()
 {
     bool runCalendarApp = true;
     long calendar[100][12][42] = {0};
-    string appointments[100][12][42];
-    for (int i = 0; i < 100; i++) {
-        for (int j = 0; j < 12; j++) {
-            for (int l = 0; l < 42; l++) {
-                appointments[i][j][l] = "XX";
-            }
-        }
-    }
     int startDay = 6; // Saturday 1/1/2000
     int lastDay = 33; // Friday 12/31/1999
     int numDaysInEachMonth[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
@@ -140,10 +183,10 @@ int main()
     int mainMenuChoice, monthSelectMenuChoice, monthMenuChoice;
     int apptDayInput;
     string apptInfo;
+    vector<string> vecApptInfo;
     bool isInMonthMenu = false;
     
-    // Generate calendar array
-    // Todo: break this out into a function that returns the array
+    // Populate the calendar array
      for (int year=2000; year < 2100; year++) {
         if (leapYearCounter == 3) {
             isLeapYear = true;
@@ -162,7 +205,7 @@ int main()
                 numDaysInCurrentMonth = numDaysInEachMonth[month];
             }
             
-            // get the number of days that the previous month contained 
+            // this gets the number of days from the previous month 
             if (month == 0) {
                 previousNumDaysInCurrentMonth = numDaysInEachMonth[11];
             }
@@ -216,7 +259,7 @@ int main()
                 cin >> monthSelectMenuChoice;
                 
                 while (isInMonthMenu) {
-                    printCalendarMonth(yearInput, monthSelectMenuChoice, calendar, appointments);
+                    printCalendarMonth(yearInput, monthSelectMenuChoice, calendar);
             
                     printMonthMenu();
                     cin >> monthMenuChoice;
@@ -224,7 +267,7 @@ int main()
                     if (monthMenuChoice == 1) {
                         // Print month calendar
                         
-                        printCalendarMonth(yearInput, monthSelectMenuChoice, calendar, appointments);
+                        printCalendarMonth(yearInput, monthSelectMenuChoice, calendar);
                         
                         cout << "\n\nType a date to add an appointment: ";
                         cin >> apptDayInput;
@@ -233,7 +276,10 @@ int main()
                         cin.ignore();
                         getline (cin, apptInfo);
                         
-                        appointments[yearInput-2000][monthSelectMenuChoice-1][apptDayInput] = apptInfo;
+                        
+                       
+                        isInMonthMenu = false;
+                        runCalendarApp = false;
                         
                     }
                     if (monthMenuChoice == 2) {
